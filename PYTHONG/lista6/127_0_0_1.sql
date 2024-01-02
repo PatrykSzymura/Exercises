a16e36047a84e6a9de45e58cbffd4692e3fde259
PatrykSzymura <121195581+PatrykSzymura@users.noreply.github.com>
2024-01-02 23:21:44 +0100

    .

---
+++ PYTHONG/lista6/127_0_0_1.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 02, 2024 at 11:21 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `students`
+--
+CREATE DATABASE IF NOT EXISTS `students` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `students`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+CREATE TABLE `album` (
+  `StudentAlbum` int(11) NOT NULL,
+  `Subject` int(11) NOT NULL,
+  `Grade` int(11) NOT NULL,
+  `Date` int(11) NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students_data`
+--
+
+CREATE TABLE `students_data` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(20) NOT NULL,
+  `Surname` varchar(20) NOT NULL,
+  `Adress` varchar(30) NOT NULL,
+  `PESEL` varchar(11) NOT NULL,
+  `Album` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `TeacherId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`StudentAlbum`),
+  ADD UNIQUE KEY `Subject` (`Subject`);
+
+--
+-- Indexes for table `students_data`
+--
+ALTER TABLE `students_data`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Album` (`Album`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Name` (`Name`),
+  ADD UNIQUE KEY `TeacherId` (`TeacherId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `students_data`
+--
+ALTER TABLE `students_data`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `students_data`
+--
+ALTER TABLE `students_data`
+  ADD CONSTRAINT `students_data_ibfk_1` FOREIGN KEY (`Album`) REFERENCES `album` (`StudentAlbum`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
